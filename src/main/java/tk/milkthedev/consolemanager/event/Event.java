f501250daeaf90bd9e733cd6507394b3cfd4f38e
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-08-11 21:13:46 +0200

    Added static function too

--- src/main/java/tk/milkthedev/consolemanager/event/Event.java
+++ src/main/java/tk/milkthedev/consolemanager/event/Event.java
@@ -4,10 +4,18 @@ public abstract class Event {
 
     /**
      * <p>Gets the handler container of the event.</p>
-     * <p><b>Should be implemented in each event class!</b></p>
      * @return
      */
     public HandlerContainer getHandlerContainer() {
+        return getHandlers();
+    }
+
+    /**
+     * <p>Gets the handler container of the event.</p>
+     * <p><b>Should be implemented in each event class!</b></p>
+     * @return
+     */
+    public static HandlerContainer getHandlers() {
         return null;
     }
 
