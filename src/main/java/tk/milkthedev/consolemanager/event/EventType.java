1c97f481539643a30c89ec4be4ce5b225d399468
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-08-11 20:38:25 +0200

    Finished adding event type

--- src/main/java/tk/milkthedev/consolemanager/event/EventType.java
+++ src/main/java/tk/milkthedev/consolemanager/event/EventType.java
@@ -1,12 +1,25 @@
 package tk.milkthedev.consolemanager.event;
 
+import tk.milkthedev.consolemanager.event.impl.CommandPreprocessEvent;
+import tk.milkthedev.consolemanager.event.impl.ConsoleInputEvent;
+
 /**
  * <p>Types of events in the ConsoleManager. Used for various optimizations</p>
  * @since 1.0.0
  */
 public enum EventType {
 
-    COMMAND_PREPROCESS,
-    CONSOLE_INPUT;
+    COMMAND_PREPROCESS(CommandPreprocessEvent.class),
+    CONSOLE_INPUT(ConsoleInputEvent.class);
+
+    private Class<? extends Event> clazz;
+
+    private EventType(Class<? extends Event> clazz) {
+        this.clazz = clazz;
+    }
+
+    public Class<? extends Event> getClazz() {
+        return clazz;
+    }
 
 }
