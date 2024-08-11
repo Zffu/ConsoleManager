1c21b7f935cc0bf167ead455f1096d937ffd881c
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-11 14:14:09 +0530

    added listeners to api

--- src/main/java/tk/milkthedev/consolemanager/api/Manager.java
+++ src/main/java/tk/milkthedev/consolemanager/api/Manager.java
@@ -1,24 +1,31 @@
 package tk.milkthedev.consolemanager.api;
 
 import tk.milkthedev.consolemanager.event.EventManager;
+import tk.milkthedev.consolemanager.event.listener.Listener;
 import tk.milkthedev.consolemanager.handler.ConsoleHandler;
 import tk.milkthedev.consolemanager.manager.command.CommandManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Manager {
     private final CommandManager commandManager;
     private final ConsoleHandler consoleHandler;
     private final EventManager eventManager;
+    private final ArrayList<Listener> listeners;
 
     public Manager() {
         this.commandManager = new CommandManager();
         this.consoleHandler = new ConsoleHandler();
         this.eventManager = new EventManager();
+        this.listeners = new ArrayList<>();
     }
 
-    public Manager(CommandManager commandManager, ConsoleHandler consoleHandler, EventManager eventManager) {
+    public Manager(CommandManager commandManager, ConsoleHandler consoleHandler, EventManager eventManager, ArrayList<Listener> listeners) {
         this.commandManager = commandManager;
         this.consoleHandler = consoleHandler;
         this.eventManager = eventManager;
+        this.listeners = listeners;
     }
 
     public CommandManager getCommandManager() {
@@ -31,5 +38,17 @@ public class Manager {
 
     public EventManager getEventManager() {
         return this.eventManager;
+    }
+
+    public void registerListener(Listener listener) {
+        this.listeners.add(listener);
+    }
+
+    public void unregisterListener(Listener listener) {
+        this.listeners.remove(listener);
+    }
+
+    public List<Listener> getListeners() {
+        return this.listeners;
     }
 }
