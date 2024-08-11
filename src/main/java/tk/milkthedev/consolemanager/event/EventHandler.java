c70d6c7d3db867a0dd88496ace1262cb45290d4e
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-11 16:28:17 +0530

    added event firing

--- src/main/java/tk/milkthedev/consolemanager/event/EventHandler.java
+++ src/main/java/tk/milkthedev/consolemanager/event/EventHandler.java
@@ -11,6 +11,22 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EventHandler {
+
+    /**
+     * Define the priority of the event.
+     * <p>
+     * First priority to the last priority executed:
+     * <ol>
+     * <li>LOWEST
+     * <li>LOW
+     * <li>NORMAL
+     * <li>HIGH
+     * <li>HIGHEST
+     * <li>MONITOR
+     * </ol>
+     */
+    EventPriority priority() default EventPriority.NORMAL;
+
     /**
      * Define if the handler ignores a cancelled event.
      * <p>
--- src/main/java/tk/milkthedev/consolemanager/event/EventManager.java
+++ src/main/java/tk/milkthedev/consolemanager/event/EventManager.java
@@ -2,14 +2,21 @@ package tk.milkthedev.consolemanager.event;
 
 import tk.milkthedev.consolemanager.event.listener.Listener;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class EventManager {
     private final ArrayList<Event> events;
     private final ArrayList<Listener> listeners;
+    private final ArrayList<EventHandler> handlers;
+    private final ArrayList<Method> methods;
     public EventManager() {
         this.events = new ArrayList<>();
         this.listeners = new ArrayList<>();
+        this.handlers = new ArrayList<>();
+        this.methods = new ArrayList<>();
     }
 
     public void registerEvent(Event event) {
@@ -22,6 +29,13 @@ public class EventManager {
 
     public void registerListener(Listener listener) {
         listeners.add(listener);
+        for(Method method : listener.getClass().getMethods()) {
+            EventHandler handler = method.getAnnotation(EventHandler.class);
+            if(handler != null) {
+                handlers.add(handler);
+                methods.add(method);
+            }
+        }
     }
 
     public void unregisterListener(Listener listener) {
@@ -29,5 +43,23 @@ public class EventManager {
     }
 
     public void fireEvent(Event event) {
+        ArrayList<Event> events1 = new ArrayList<>();
+        ArrayList<EventHandler> handlers1 = new ArrayList<>();
+
+        events.forEach(e -> {
+            if (e.getEventName().equals(event.getEventName())) {
+                events1.add(e);
+                handlers1.add(handlers.get(events.indexOf(e)));
+            }
+        });
+
+        // TODO: Sort on the basis of priority
+        for (Method method : methods) {
+            try {
+                method.invoke(handlers1, handlers);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
