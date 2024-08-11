407991735977264781bcda1419c0d032928c8691
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-11 14:32:59 +0530

    made command manager

--- src/main/java/tk/milkthedev/consolemanager/manager/command/Command.java
+++ src/main/java/tk/milkthedev/consolemanager/manager/command/Command.java
@@ -1,4 +1,20 @@
 package tk.milkthedev.consolemanager.manager.command;
 
-public interface Command {
+public abstract class Command {
+    private final String name;
+    private final String description;
+    public Command(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract void execute(String[] args);
 }
--- src/main/java/tk/milkthedev/consolemanager/manager/command/CommandManager.java
+++ src/main/java/tk/milkthedev/consolemanager/manager/command/CommandManager.java
@@ -1,4 +1,44 @@
 package tk.milkthedev.consolemanager.manager.command;
 
+import tk.milkthedev.consolemanager.api.Manager;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CommandManager {
+    private final ArrayList<Command> commands;
+
+    public CommandManager() {
+        this.commands = new ArrayList<>();
+    }
+
+    public void registerCommand(Command command) {
+        commands.add(command);
+    }
+
+    public void unregisterCommand(Command command) {
+        commands.remove(command);
+    }
+
+    public boolean executeCommand(String input) {
+        String[] parts = input.split(" ");
+        String commandName = parts[0];
+        Command command = getCommand(commandName);
+
+        if (command == null) return false;
+
+        // TODO: Fire CommandPreprocessEvent
+        command.execute(Arrays.copyOfRange(parts, 1, parts.length));
+        return true;
+    }
+
+    public Command getCommand(String name) {
+        for (Command command : commands) {
+            if (command.getName().equalsIgnoreCase(name)) {
+                return command;
+            }
+        }
+        return null;
+    }
 }
