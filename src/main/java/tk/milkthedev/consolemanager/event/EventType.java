5a4a073b28e540d24e256750d20514a3040810c4
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-08-11 20:37:23 +0200

    Added event type

---
+++ src/main/java/tk/milkthedev/consolemanager/event/EventType.java
@@ -0,0 +1,12 @@
+package tk.milkthedev.consolemanager.event;
+
+/**
+ * <p>Types of events in the ConsoleManager. Used for various optimizations</p>
+ * @since 1.0.0
+ */
+public enum EventType {
+
+    COMMAND_PREPROCESS,
+    CONSOLE_INPUT;
+
+}
